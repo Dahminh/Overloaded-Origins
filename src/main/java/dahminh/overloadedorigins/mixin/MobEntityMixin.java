d52fdc3e8cf6b6301931a1bf3189954b6d24b7c9
Dahminh <dnvu@ucsd.edu>
2023-06-11 17:18:30 -0700

    Changed from More Origins to Overloaded Origins and a bunch of changes

---
+++ src/main/java/dahminh/overloadedorigins/mixin/MobEntityMixin.java
@@ -0,0 +1,27 @@
+package dahminh.overloadedorigins.mixin;
+
+import dahminh.overloadedorigins.effect.CustomEffects;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin extends LivingEntity {
+    protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @ModifyVariable(method = "setTarget", at = @At("HEAD"))
+    private LivingEntity modifyTarget(LivingEntity target) {
+        if (world.isClient() || !(target instanceof PlayerEntity)) {
+            return target;
+        }
+
+        return target.hasStatusEffect(CustomEffects.SHADOWCLOAK) ? null : target;
+    }
+}
