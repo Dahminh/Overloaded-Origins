5f114ca24f9e05dfc83c94b4d7fda00d9c8f574c
Dahminh <dnvu@ucsd.edu>
2024-08-13 03:31:16 -0700

    Refactored Overloaded Origins' Checks for checking origins and their evolutions

--- src/main/java/dahminh/overloadedorigins/util/Checks.java
+++ src/main/java/dahminh/overloadedorigins/util/Checks.java
@@ -7,22 +7,39 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Checks {
-    private static final Identifier assassinT0 = new Identifier("assassin:upgraded0");
-    private static final Identifier assassinT1 = new Identifier("assassin:upgraded1");
-    private static final Identifier assassinT2 = new Identifier("assassin:upgraded2");
-    public static Boolean assassinOriginCheck(LivingEntity target) {
+
+    public static enum OriginsTiers {
+        ASSASSIN(new Identifier("assassin:upgraded0"), new Identifier("assassin:upgraded1"), new Identifier("assassin:upgraded2")),
+        PHANTOM(new Identifier("phantom:upgraded0"), new Identifier("phantom:upgraded1"), new Identifier("phantom:upgraded2"));
+
+        private Identifier tierOne;
+        private Identifier tierTwo;
+        private Identifier tierThree;
+        OriginsTiers(Identifier tierOne, Identifier tierTwo, Identifier tierThree) {
+            this.tierOne   = tierOne;
+            this.tierTwo   = tierTwo;
+            this.tierThree = tierThree;
+        }
+
+        public boolean checkOriginIdentifier(Identifier input) {
+            return input.equals(this.tierOne) || input.equals(this.tierTwo) || input.equals(this.tierThree);
+        }
+
+    }
+
+    public static Boolean originCheck(LivingEntity target, OriginsTiers origin) {
         if (!(target instanceof PlayerEntity)) return false;
         OriginComponent component = ModComponents.ORIGIN.get(target);
-        return component.getOrigins().values().stream().anyMatch(o ->
-                o.getIdentifier().equals(assassinT0) ||
-                        o.getIdentifier().equals(assassinT1) ||
-                        o.getIdentifier().equals(assassinT2));
+        return component.getOrigins().values().stream().anyMatch(o -> origin.checkOriginIdentifier(o.getIdentifier()));
     }
 
     public static Boolean assassinInvisibilityCheck(LivingEntity target) {
         if (!(target instanceof PlayerEntity)) return false;
-        return     assassinOriginCheck(target)
+        return     originCheck(target, OriginsTiers.ASSASSIN)
                 && target.hasStatusEffect(StatusEffects.INVISIBILITY)
                 && target.getStatusEffect(StatusEffects.INVISIBILITY).getAmplifier() >= 4;
     }
